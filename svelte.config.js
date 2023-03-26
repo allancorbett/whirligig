f2de70a7908f44977e068ad2c05cefce910538fc
Allan Corbett <allan@intelligentgrowthsolutions.com>
2023-03-26 16:57:10 +0100

    Update url of site

--- svelte.config.js
+++ svelte.config.js
@@ -5,13 +5,11 @@ const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : '/whirligig',
+			base: dev ? '' : 'whirligig',
 		}
 	}
 };
